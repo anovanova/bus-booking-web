c7cf32d2e1c520e13da430bf1aecae60a6db3ed2
macedera <michael.acedera@fasttrackph.com>
2025-11-05 16:01:16 +0800

    feat(): add auth

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,10 +1,18 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
-export async function GET(req: Request) {
+export async function POST(req: NextRequest) {
   const backendURL = process.env.BACKEND_URL;
+  const body = await req.json();
   try {
-    const response = await fetch(`${backendURL}/login`);
-    return NextResponse.json({ response }, { status: 200 });
+    const response = await fetch(`${backendURL}/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
+    return NextResponse.json({ response: data }, { status: 200 });
   } catch (err) {
     return NextResponse.json({ error: "failed to load data" }, { status: 500 });
   }
