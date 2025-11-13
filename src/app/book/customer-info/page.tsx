c17f719a575df941b09df22cc8e06fe1c2667a3c
macedera <michael.acedera@fasttrackph.com>
2025-11-13 14:44:47 +0800

    chore(): remove unused package

--- src/app/book/customer-info/page.tsx
+++ src/app/book/customer-info/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
